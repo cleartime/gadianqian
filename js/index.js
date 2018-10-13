633e7e6feae79eaec90c1972783d55533a00547e
cleartime <562606139@qq.com>
2018-10-13 19:49:28 +0800

    init

--- js/index.js
+++ js/index.js
@@ -2,28 +2,150 @@ new Vue({
 	 el: '#app',
 	data: function(){
 		return {
+			home: {
+				banerList: [],
+				hotRecoList: [],
+				popupsList: [],
+				productList: []
+			},
 			visite: {
 				alert: false,
-				login: true,
-				mask: true
+				login: false,
+				mask: false,
 			},
 			tipIconSwitch: false,
-			tipIconUrl: `/images/${this.tipIconSwitch ? 'succ' : 'kong'}.png`,
+			picImgUrl: '',
+			picImg: {},
+			isSend: false,
+			picVerifyCode: '',
+			num: 59,
+			login: {
+				mobilePhone: '',
+				smsCode: '',
+				flag: 'MSG_REG',
+				from: 1,
+				device : ''
+			}
+		}
+	},
+	computed: {
+		'user': function(){
+			return localStorage.user 
+			? JSON.parson(localStorage.user )
+			: {juid:''}
+		},
+		'tipIconUrl': function(){
+			return `/images/${this.tipIconSwitch ? 'succ' : 'kong'}.png`
 		}
 	},
 	created: function (argument) {
+		this.init();
 	},
 	mounted: function(){
-
+		this.getPicCodeApi()
 	},
 	methods: {
+		init: function(){
+			http('get', getHomeInfoApi, null, function(res){
+				this.home = res;
+				if (this.home.popupsList.length) {
+					this.visite.mask = true
+					this.visite.alert = true
+				}
+				this.$nextTick(function(){
+					this.banner()
+				})
+			}.bind(this))
+		},
+		linkto: function(item, type){
+			if (!this.user.juid) {
+				this.visite.mask = false
+				this.visite.alert = false
+				this.visite.login = true
+				return
+			}
+			var data = {
+				productId: item.productId || '',
+				position: type,
+				from: 1,
+				linkUrl: item.linkUrl
+			}
+			http('get', getHomeInfoApi, data)
+			if (item.linkUrl) {
+				location.href = item.linkUrl;
+			}
+		},
+		banner(){
+			new Swiper('.swiper-container', {
+				autoplay: true,
+			    loop: true, // 循环模式选项
+			    disableOnInteraction: false,
+			    pagination: {
+			      el: '.swiper-pagination',
+			    }
+			 })
+		},
 		close: function(){
 			this.visite.alert = false;
 			this.visite.login = false;
 			this.visite.mask = false;
 		},
+		getPicCodeApi: function(){
+			http('get', getPicCodeApi, null, function(res){
+				this.picImg = res;
+				this.picImgUrl = res.picVerifyUrl +'?'+ res.picId;
+			}.bind(this))
+		},
+		sendSMSCodeApi: function(){
+			if(this.isSend) return
+			if(!this.login.mobilePhone){
+				this.$toast.center('手机号码不能为空');
+				return
+			}
+			if(!this.picVerifyCode){
+				this.$toast.center('图形验证码不能为空');
+				return
+			}
+			this.isSend = true
+			var time = setInterval(function() {
+				while(!this.num--){
+					clearInterval(time)
+					this.isSend = false
+					this.num = 59
+				}
+			}.bind(this), 1000);
+			var data = {
+				picId: this.picImg.picId,
+				picVerifyCode: this.picVerifyCode,
+				flag: "MSG_REG",
+				mobilePhone: this.login.mobilePhone
+			}
+			http('post', sendSMSCodeApi	, data, function(res){
+				this.picImgUrl = res.picVerifyUrl +'?'+ res.picId;
+			}.bind(this))
+		},
 		tipIcon: function(){
-			this.tipIconSwitch=!this.tipIconSwitch
+			this.tipIconSwitch = !this.tipIconSwitch
+		},
+		submit: function(){
+			if(!this.login.mobilePhone){
+				this.$toast.center('手机号码不能为空');
+				return
+			}
+			if(!this.picVerifyCode){
+				this.$toast.center('图形验证码不能为空');
+				return
+			}
+			if(!this.login.smsCode){
+				this.$toast.center('验证码不能为空');
+				return
+			}
+			http('get', regAndLogin	, this.login, function(res){
+				this.user = res
+				localStorage.user = JSON.stringify(res)
+			}.bind(this))
 		}
+	},
+	watch: {
 	}
 })
---
+++ js/until.js
@@ -0,0 +1,25 @@
+window.log = console.log
+var baseUrl = 'http://47.104.242.171:90'
+var getHomeInfoApi = baseUrl + '/aosuite/api/esb/user/getHomeInfoApi?page=1&pageSize=2';
+var saveJumpRecord = baseUrl + '/aosuite/api/esb/user/saveJumpRecord';
+var getPicCodeApi = baseUrl + '/aosuite/api/esb/captcha/getPicCodeApi';
+var sendSMSCodeApi = baseUrl + '/aosuite/api/esb/sms/sendSMSCodeApi';
+var regAndLogin = baseUrl + '/aosuite/api/esb/user/regAndLogin';
+Vue.use(Toast);
+function http(type, url, data, ca){
+	$.ajax({
+	  type: type,
+	  url: url,
+	  data: type === 'post' ? JSON.stringify(data) : data,
+	  contentType: 'application/json',
+	  success: success,
+	  dataType: 'json'
+	});
+	function success(res){
+		if(!res.data){
+			Vue.prototype.$toast.center(res.friendErrMsg);
+			return 
+		}
+		return ca && ca(res.data)
+	}
+}
